00088f492319af0bfc5d6dc22e8b519b67ec5c6e
Daniel Ramírez-Chiquillo <hi@danielrachi.com>
2024-10-12 07:12:46 -0500

    Capture input text on change
    Added logic that sends the current input to the server every time it
    changes.
    
    This will be useful later for turning the input into a QR code.

--- lib/qr_web/live/qr_live.ex
+++ lib/qr_web/live/qr_live.ex
@@ -4,4 +4,9 @@ defmodule QrWeb.QrLive do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
+
+  def handle_event("input_changed", %{"qr_input" => input_text}, socket) do
+    IO.puts(input_text)
+    {:noreply, socket}
+  end
 end
